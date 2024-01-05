aa3d6afe614131045b4bd1d2d8a07947ac2bf93d
juneyoung Lee <junyeong237@gmail.com>
2024-01-05 14:59:00 +0900

    Feat : Global Exception Handler 추가

--- src/main/java/com/example/booktalk/global/exception/GlobalException.java
+++ src/main/java/com/example/booktalk/global/exception/GlobalException.java
@@ -1,15 +1,12 @@
 package com.example.booktalk.global.exception;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
-public class GlobalException extends RuntimeException{
+@RequiredArgsConstructor
+public class GlobalException extends RuntimeException {
 
     private final ErrorCode errorCode;
-
-    public GlobalException(ErrorCode errorCode) {
-        super(errorCode.getMessage());
-        this.errorCode = errorCode;
-    }
 
 }
