5851bf3995e6aa98caa486325786b0022beddc31
juneyoung Lee <junyeong237@gmail.com>
2024-01-05 15:00:16 +0900

    [#1] Feat : Product CRUD 추가

--- src/main/java/com/example/booktalk/domain/product/entity/Product.java
+++ src/main/java/com/example/booktalk/domain/product/entity/Product.java
@@ -1,13 +1,17 @@
 package com.example.booktalk.domain.product.entity;
 
 import com.example.booktalk.domain.common.BaseEntity;
+import com.example.booktalk.domain.product.dto.request.ProductRegisterReq;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.util.List;
 import lombok.AccessLevel;
@@ -41,13 +45,26 @@ public class Product extends BaseEntity {
     @Column(nullable = false)
     private Boolean finished;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
     @Builder
-    private Product(String name, Long price, Long quantity, List<Region> regions) {
+    private Product(String name, Long price, Long quantity, List<Region> regions, User user) {
         this.name = name;
         this.quantity = quantity;
         this.price = price;
         this.regions = regions;
+        this.user = user;
         this.finished = false;
+    }
+
+    public void update(ProductRegisterReq req) {
+        this.name = req.name();
+        this.quantity = req.quantity();
+        this.price = req.price();
+        this.regions = req.regions();
+        this.finished = req.finished();
     }
 
     public void finish() { //거래 상태를 완료로 변경
--- src/main/java/com/example/booktalk/domain/product/repository/ProductRepository.java
+++ src/main/java/com/example/booktalk/domain/product/repository/ProductRepository.java
@@ -2,8 +2,11 @@ package com.example.booktalk.domain.product.repository;
 
 
 import com.example.booktalk.domain.product.entity.Product;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findAll();
 
 }
