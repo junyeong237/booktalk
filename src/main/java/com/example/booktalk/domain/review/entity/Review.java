d330d5a6ee6944688ebe7d5aa418c6805c30a4e5
guxo2795 <guxo1211@gmail.com>
2024-01-05 13:01:14 +0900

    Feat : Review Entity 추가

---
+++ src/main/java/com/example/booktalk/domain/review/entity/Review.java
@@ -0,0 +1,31 @@
+package com.example.booktalk.domain.review.entity;
+
+
+import com.example.booktalk.domain.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tb_review")
+public class Review extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String content;
+
+    @Builder
+    private Review(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+}
