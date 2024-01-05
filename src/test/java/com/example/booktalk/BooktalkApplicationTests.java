41f86a723687df58e551253c3a6f7b974b259908
juneyoung Lee <junyeong237@gmail.com>
2024-01-05 10:36:21 +0900

    init commit

---
+++ src/test/java/com/example/booktalk/BooktalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.booktalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooktalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
