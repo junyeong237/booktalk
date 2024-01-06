f7144b947e0a49e1311e43daf34348048394812e
guxo2795 <guxo1211@gmail.com>
2024-01-06 19:31:17 +0900

    [#2] Refactor : entity, controller 수정

--- src/main/java/com/example/booktalk/domain/review/controller/ReviewController.java
+++ src/main/java/com/example/booktalk/domain/review/controller/ReviewController.java
@@ -1,11 +1,10 @@
 package com.example.booktalk.domain.review.controller;
 
+import com.example.booktalk.domain.review.dto.request.ReviewCreateReq;
 import com.example.booktalk.domain.review.dto.request.ReviewUpdateReq;
 import com.example.booktalk.domain.review.dto.response.*;
-import com.example.booktalk.domain.review.dto.request.ReviewCreateReq;
 import com.example.booktalk.domain.review.service.ReviewService;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
--- src/main/java/com/example/booktalk/domain/review/entity/Review.java
+++ src/main/java/com/example/booktalk/domain/review/entity/Review.java
@@ -1,13 +1,18 @@
 package com.example.booktalk.domain.review.entity;
 
 
+import com.example.booktalk.domain.comment.entity.Comment;
 import com.example.booktalk.domain.common.BaseEntity;
 import com.example.booktalk.domain.review.dto.request.ReviewUpdateReq;
+import com.example.booktalk.domain.reviewlike.entity.ReviewLike;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -22,6 +27,16 @@ public class Review extends BaseEntity {
     private String title;
     @Column(nullable = false)
     private String content;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "user_id")
+//    private User user;
+
+    @OneToMany(mappedBy = "review", cascade = CascadeType.REMOVE)
+    private List<Comment> commentList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "review", cascade = CascadeType.REMOVE)
+    private List<ReviewLike> reviewLikeList = new ArrayList<>();
 
     @Builder
     private Review(String title, String content) {
