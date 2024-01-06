5b876801f8b89b4db5b91b0685ec5a4c3f532afe
guxo2795 <guxo1211@gmail.com>
2024-01-06 20:27:07 +0900

    [#8] Feat : toggle 좋아요 기능 추가

--- src/main/java/com/example/booktalk/domain/review/entity/Review.java
+++ src/main/java/com/example/booktalk/domain/review/entity/Review.java
@@ -1,13 +1,19 @@
 package com.example.booktalk.domain.review.entity;
 
 
+import com.example.booktalk.domain.comment.entity.Comment;
 import com.example.booktalk.domain.common.BaseEntity;
 import com.example.booktalk.domain.review.dto.request.ReviewUpdateReq;
+import com.example.booktalk.domain.reviewlike.entity.ReviewLike;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -23,6 +29,17 @@ public class Review extends BaseEntity {
     @Column(nullable = false)
     private String content;
 
+    @ColumnDefault("0")
+    @Column
+    private Integer reviewLikeCount;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "user_id")
+//    private User user;
+
+    @OneToMany(mappedBy = "review", cascade = CascadeType.REMOVE)
+    private List<Comment> commentList = new ArrayList<>();
+
     @Builder
     private Review(String title, String content) {
         this.title = title;
@@ -32,5 +49,15 @@ public class Review extends BaseEntity {
     public void update(ReviewUpdateReq req) {
         this.title = req.title();
         this.content = req.content();
+    }
+
+    public void increaseReviewLike() {
+        reviewLikeCount++;
+    }
+
+    public void decreaseReviewLike() {
+        if(reviewLikeCount > 0) {
+            reviewLikeCount--;
+        }
     }
 }
