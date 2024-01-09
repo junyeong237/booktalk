fe70173697703cd93b0753b5a6eba31b7fdfedfa
juneyoung Lee <junyeong237@gmail.com>
2024-01-10 00:51:18 +0900

    [#25] Feat : Chat Dto

---
+++ src/main/java/com/example/booktalk/domain/chat/dto/response/ChatEnterRes.java
@@ -0,0 +1,5 @@
+package com.example.booktalk.domain.chat.dto.response;
+
+public record ChatEnterRes(String message) {
+
+}
---
+++ src/main/java/com/example/booktalk/domain/chat/dto/response/ChatExitRes.java
@@ -0,0 +1,5 @@
+package com.example.booktalk.domain.chat.dto.response;
+
+public record ChatExitRes(String message) {
+
+}
