9bac12b54cd5cf4cb0b8c664275938bd5c71b46c
guxo2795 <guxo1211@gmail.com>
2024-01-10 14:27:17 +0900

    [#28] Feat : 유저신고 controller, service 추가 및 유저신고 기능

---
+++ src/main/java/com/example/booktalk/domain/userreport/service/UserReportService.java
@@ -0,0 +1,55 @@
+package com.example.booktalk.domain.userreport.service;
+
+import com.example.booktalk.domain.user.entity.User;
+import com.example.booktalk.domain.user.exception.NotFoundUserException;
+import com.example.booktalk.domain.user.exception.UserErrorCode;
+import com.example.booktalk.domain.user.repository.UserRepository;
+import com.example.booktalk.domain.userreport.dto.request.UserReportCreateReq;
+import com.example.booktalk.domain.userreport.dto.response.UserReportCreateRes;
+import com.example.booktalk.domain.userreport.entity.UserReport;
+import com.example.booktalk.domain.userreport.exception.NotFoundReportedUserException;
+import com.example.booktalk.domain.userreport.exception.NotPermissionSelfReportException;
+import com.example.booktalk.domain.userreport.exception.UserReportErrorCode;
+import com.example.booktalk.domain.userreport.repository.UserReportRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserReportService {
+
+    private final UserReportRepository userReportRepository;
+    private final UserRepository userRepository;
+
+    public UserReportCreateRes createUserReport(UserReportCreateReq req, Long userId) {
+
+        User user = findUser(userId);
+        User reportedUser = findReportedUser(req.reportedUserId());
+
+        if(user.getId().equals(reportedUser.getId())) {
+            throw new NotPermissionSelfReportException(UserReportErrorCode.NOT_PERMISSION_SELF_REPORT);
+        }
+
+        UserReport userReport = UserReport.builder()
+                .reason(req.reason())
+                .reportedUser(reportedUser)
+                .user(user)
+                .build();
+
+        userReportRepository.save(userReport);
+
+        return UserReportCreateRes.builder()
+                .msg("신고가 완료되었습니다.")
+                .build();
+    }
+
+    private User findReportedUser(Long reportedUserId) {
+        return userRepository.findById(reportedUserId)
+                .orElseThrow(() -> new NotFoundReportedUserException(UserReportErrorCode.NOT_FOUND_REPORTED_USER));
+    }
+
+    private User findUser(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundUserException(UserErrorCode.NOT_FOUND_USER));
+    }
+}
