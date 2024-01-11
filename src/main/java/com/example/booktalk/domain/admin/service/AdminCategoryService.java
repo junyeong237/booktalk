7b096b88eb137b4d6eb3729bd2b8c7b9a9e769af
juneyoung Lee <junyeong237@gmail.com>
2024-01-11 16:19:52 +0900

    [#41] Refactor : Category수정

--- src/main/java/com/example/booktalk/domain/admin/service/AdminCategoryService.java
+++ src/main/java/com/example/booktalk/domain/admin/service/AdminCategoryService.java
@@ -9,7 +9,6 @@ import com.example.booktalk.domain.category.entity.Category;
 import com.example.booktalk.domain.category.exception.CategoryErrorCode;
 import com.example.booktalk.domain.category.exception.ExistCategoryException;
 import com.example.booktalk.domain.category.exception.ForbiddenDeleteCategoryException;
-import com.example.booktalk.domain.category.exception.NotFoundCategoryException;
 import com.example.booktalk.domain.category.repository.CategoryRepository;
 import com.example.booktalk.domain.productcategory.repository.ProductCategoryRepository;
 import lombok.RequiredArgsConstructor;
@@ -33,22 +32,22 @@ public class AdminCategoryService {
 
         category = categoryRepository.save(category);
 
-        return new CategoryCreateRes(category.getId(),category.getName());
+        return new CategoryCreateRes(category.getId(), category.getName());
     }
 
 
     public CategoryUpdateRes updateCategory(
         Long categoryId,
         CategoryUpdateReq req) {
-        Category category = findCategory(categoryId);
+        Category category = categoryRepository.findCategoryByIdWithThrow(categoryId);
         validateDuplicateCategory(req.name());
         category.update(req.name());
-        return new CategoryUpdateRes(category.getName());
+        return new CategoryUpdateRes(category.getId(), category.getName());
     }
 
 
     public CategoryDeleteRes deleteCategory(final Long categoryId) {
-        Category category = findCategory(categoryId);
+        Category category = categoryRepository.findCategoryByIdWithThrow(categoryId);
 
         if (productCategoryRepository.existsByCategory_Id(categoryId)) {
             throw new ForbiddenDeleteCategoryException(CategoryErrorCode.FORBIDDEM_DELETE_CATEGORY);
@@ -56,11 +55,6 @@ public class AdminCategoryService {
 
         categoryRepository.delete(category);
         return new CategoryDeleteRes("카테고리가 삭제가 완료되었습니다.");
-    }
-
-    private Category findCategory(Long categoryId) {
-        return categoryRepository.findById(categoryId)
-            .orElseThrow(() -> new NotFoundCategoryException(CategoryErrorCode.NOT_FOUND_CATEGORY));
     }
 
     private void validateDuplicateCategory(String updateName) {
