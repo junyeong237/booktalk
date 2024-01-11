656727c6a183d51911c6b44962b43402ab9b40b2
seunghosin <shin2896@naver.com>
2024-01-11 15:30:33 +0900

    [#41]Refactor: UserRepository findUserByIdWithThrow 추가

--- src/main/java/com/example/booktalk/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/booktalk/domain/user/repository/UserRepository.java
@@ -1,6 +1,11 @@
 package com.example.booktalk.domain.user.repository;
 
+import com.example.booktalk.domain.review.entity.Review;
+import com.example.booktalk.domain.review.exception.NotFoundReviewException;
+import com.example.booktalk.domain.review.exception.ReviewErrorCode;
 import com.example.booktalk.domain.user.entity.User;
+import com.example.booktalk.domain.user.exception.NotFoundUserException;
+import com.example.booktalk.domain.user.exception.UserErrorCode;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,5 +16,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByNickname(String name);
 
+    default User findUserByIdWithThrow(Long id) {
+        return findById(id).orElseThrow(() ->
+            new NotFoundUserException(UserErrorCode.NOT_FOUND_USER));
+    }
     List<User> findAll();
 }
