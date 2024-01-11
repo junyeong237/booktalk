dfcef63adae37ad4a6dc21f79a9b101a1021a84e
guxo2795 <guxo1211@gmail.com>
2024-01-11 15:56:06 +0900

    [#41] Refactor : Comment 수정

--- src/main/java/com/example/booktalk/domain/comment/dto/response/CommentGetListRes.java
+++ src/main/java/com/example/booktalk/domain/comment/dto/response/CommentGetListRes.java
@@ -1,9 +1,5 @@
 package com.example.booktalk.domain.comment.dto.response;
 
-import com.example.booktalk.domain.user.entity.User;
-import lombok.Builder;
-
-@Builder
 public record CommentGetListRes (
         Long commentId,
         String content,
