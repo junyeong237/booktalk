3cbc933432a5ff7e6e63299c34924bb2be7a55d8
seunghosin <shin2896@naver.com>
2024-01-13 17:55:59 +0900

    [#59]Feat: 회원 탈퇴 구현

--- src/main/java/com/example/booktalk/domain/user/exception/UserErrorCode.java
+++ src/main/java/com/example/booktalk/domain/user/exception/UserErrorCode.java
@@ -13,7 +13,7 @@ public enum UserErrorCode implements ErrorCode {
     NOT_FOUND_REFRESH_TOKEN(HttpStatus.NOT_FOUND, "해당 리프레시 토큰을 찾을 수 없습니다."),
     ALREADY_EXIST_EMAIL(HttpStatus.BAD_REQUEST, "이미 가입된 이메일입니다."),
     INVALID_ADMIN_CODE(HttpStatus.BAD_REQUEST, "관리자 인증 번호가 틀렸습니다."),
-    BAD_LOGIN(HttpStatus.BAD_REQUEST, "이메일 또는 패스워드를 확인해주세요."),
+    BAD_LOGIN(HttpStatus.BAD_REQUEST, "패스워드를 확인해주세요."),
     NOT_MATCH_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
     INVALID_PASSWORD_CHECK(HttpStatus.BAD_REQUEST, "비밀번호와 비밀번호 확인이 일치하지 않습니다.");
 
