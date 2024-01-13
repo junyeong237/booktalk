b62f4c585a20c1482551106e486dcc2870210c53
lee june yeong <70509488+junyeong237@users.noreply.github.com>
2024-01-14 02:28:16 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -17,4 +17,4 @@ else
 fi
 
 echo "> $JAR_PATH 배포" #3
-nohup java -jar /home/ubuntu/app/build/libs/booktalk-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar /home/ubuntu/app/build/libs/booktalk-0.0.1-SNAPSHOT.jar > $REPOSITORY/nohup.out 2>&1 &
