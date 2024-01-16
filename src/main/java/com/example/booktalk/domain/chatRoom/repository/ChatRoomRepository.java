f168a94868f355ea083645ff2253180df6212f3f
juneyoung Lee <junyeong237@gmail.com>
2024-01-17 02:50:31 +0900

    [#113] 채팅방 목록 확인 메서드 추가

--- src/main/java/com/example/booktalk/domain/chatRoom/repository/ChatRoomRepository.java
+++ src/main/java/com/example/booktalk/domain/chatRoom/repository/ChatRoomRepository.java
@@ -3,6 +3,7 @@ package com.example.booktalk.domain.chatRoom.repository;
 import com.example.booktalk.domain.chatRoom.entity.ChatRoom;
 import com.example.booktalk.domain.product.exception.NotFoundProductException;
 import com.example.booktalk.domain.product.exception.ProductErrorCode;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -11,6 +12,8 @@ public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
     Boolean existsByReceiverIdAndSenderId(Long receiverId, Long SenderId);
 
     Optional<ChatRoom> findByReceiverIdAndSenderId(Long receiverId, Long SenderId);
+
+    List<ChatRoom> findAllByReceiverIdOrSenderId(Long receiverId, Long SenderId);
 
     default ChatRoom findChatRoomByIdWithThrow(Long id) {
         return findById(id).orElseThrow(() ->
