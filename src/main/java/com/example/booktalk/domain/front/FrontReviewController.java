21f7821ac8520760901c7ec793b9dd4bee9f3974
seunghosin <shin2896@naver.com>
2024-01-17 11:59:03 +0900

    [#97]Style: 상세페이지 구현

--- src/main/java/com/example/booktalk/domain/front/FrontReviewController.java
+++ src/main/java/com/example/booktalk/domain/front/FrontReviewController.java
@@ -1,7 +1,9 @@
 package com.example.booktalk.domain.front;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class FrontReviewController {
@@ -11,7 +13,13 @@ public class FrontReviewController {
     }
 
     @GetMapping("/api/v1/reviews/post")
-    public String reviewPost() {
+    public String postReviewPage() {
         return "createReview";
+    }
+
+    @GetMapping("api/v1/reviews/detail/{reviewId}")
+    public String reviewDetailPage(@PathVariable Long reviewId, Model model) {
+        model.addAttribute("reviewId", reviewId);
+        return "reviewDetail";
     }
 }
