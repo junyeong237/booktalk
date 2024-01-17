251f8b3dc154ff3218ea471dee75ad7965dd3554
juneyoung Lee <junyeong237@gmail.com>
2024-01-17 17:32:54 +0900

    [#127] Refactor : 다른유저 프로필 조회 페이지

--- src/main/java/com/example/booktalk/domain/front/FrontUserController.java
+++ src/main/java/com/example/booktalk/domain/front/FrontUserController.java
@@ -1,14 +1,22 @@
 package com.example.booktalk.domain.front;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class FrontUserController {
 
     @GetMapping("/api/v1/users/profile")
-    public String loginPage() {
+    public String myProfilePage() {
         return "profile";
+    }
+
+    @GetMapping("/api/v1/users/{userId}/profile")
+    public String otherProfilePage(@PathVariable Long userId, Model model) {
+        model.addAttribute("userId", userId);
+        return "otherProfile";
     }
 
 }
