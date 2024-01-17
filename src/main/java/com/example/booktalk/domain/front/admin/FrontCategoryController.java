7f9a0ff506883b632567a87f8860d5e1f37ae75e
kimgunwoo <guncow0603@gmail.com>
2024-01-17 15:17:33 +0900

    Refactor : 관리자 카테고리 html 수정

--- src/main/java/com/example/booktalk/domain/front/admin/FrontCategoryController.java
+++ src/main/java/com/example/booktalk/domain/front/admin/FrontCategoryController.java
@@ -1,4 +1,4 @@
-package com.example.booktalk.domain.front;
+package com.example.booktalk.domain.front.admin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
