3978582c62ca9364d599db8a02d6e488be1d8100
juneyoung Lee <junyeong237@gmail.com>
2024-01-22 19:47:11 +0900

    Refactor : admin user 수정

--- src/main/java/com/example/booktalk/domain/admin/controller/AdminUserContoller.java
+++ src/main/java/com/example/booktalk/domain/admin/controller/AdminUserContoller.java
@@ -2,13 +2,18 @@ package com.example.booktalk.domain.admin.controller;
 
 
 import com.example.booktalk.domain.admin.service.AdminUserService;
+import com.example.booktalk.domain.user.dto.response.UserBlockRes;
 import com.example.booktalk.domain.user.dto.response.UserReportRes;
+import com.example.booktalk.domain.user.dto.response.UserUnblockRes;
+import com.example.booktalk.global.security.UserDetailsImpl;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v2/admin/users")
@@ -21,6 +26,20 @@ public class AdminUserContoller {
     public List<UserReportRes> getUser() {
         List<UserReportRes> res = adminUserService.getAllUsers();
         return res;
+    }
+
+    @PutMapping("/block/{userId}")
+    public UserBlockRes userBlock(@AuthenticationPrincipal UserDetailsImpl userDetails,
+        @PathVariable Long userId
+    ) {
+        return adminUserService.createUserBlock(userDetails.getUser().getId(), userId);
+    }
+
+    @PutMapping("/unBlock/{userId}")
+    public UserUnblockRes userUnBlock(
+        @PathVariable Long userId
+    ) {
+        return adminUserService.createUserUnBlock(userId);
     }
 
 
