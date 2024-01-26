716c732626c4d082e932ba68a6b9d3b9c1f1d3e1
juneyoung Lee <junyeong237@gmail.com>
2024-01-26 17:05:53 +0900

    Refactor : 레디스 캐싱으로 인한 차단적용

--- src/main/java/com/example/booktalk/domain/admin/service/AdminUserService.java
+++ src/main/java/com/example/booktalk/domain/admin/service/AdminUserService.java
@@ -11,6 +11,7 @@ import com.example.booktalk.domain.user.repository.UserRepository;
 import java.util.List;
 import java.util.Objects;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -31,6 +32,7 @@ public class AdminUserService {
             ).toList();
     }
 
+    @CacheEvict(value = "user", key = "#userId")
     public UserBlockRes createUserBlock(Long myUserId, Long userId) {
         User user = userRepository.findUserByIdWithThrow(userId);
         if (Objects.equals(myUserId, userId)) {
