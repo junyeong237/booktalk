2fcc13d45c53e1672acf4841b7f7007014102368
juneyoung Lee <junyeong237@gmail.com>
2024-01-26 23:56:06 +0900

    Refactor : 프로필 수정시 비밀번호 입력하지않도록 수정

--- src/main/java/com/example/booktalk/domain/user/dto/request/UserProfileReq.java
+++ src/main/java/com/example/booktalk/domain/user/dto/request/UserProfileReq.java
@@ -3,11 +3,10 @@ package com.example.booktalk.domain.user.dto.request;
 import jakarta.validation.constraints.Pattern;
 
 public record UserProfileReq(
-    String password,
     String nickname,
     String location,
     String description,
-    @Pattern(regexp = "^[0-9]+(-[0-9]+)*$",message = "전화번호 양식을 맞춰주세요.")
+    @Pattern(regexp = "^[0-9]+(-[0-9]+)*$", message = "전화번호 양식을 맞춰주세요.")
     String phone
 ) {
 
