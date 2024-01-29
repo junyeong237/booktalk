6b603dadb37a8715195eebec834b64eae0262fbb
guxo2795 <guxo1211@gmail.com>
2024-01-29 18:53:54 +0900

    refactor : 프로필 수정 시, 전화번호 없이 수정 가능

--- src/main/java/com/example/booktalk/domain/user/dto/request/UserProfileReq.java
+++ src/main/java/com/example/booktalk/domain/user/dto/request/UserProfileReq.java
@@ -6,7 +6,7 @@ public record UserProfileReq(
     String nickname,
     String location,
     String description,
-    @Pattern(regexp = "^[0-9]+(-[0-9]+)*$", message = "전화번호 양식을 맞춰주세요.")
+    @Pattern(regexp = "^$|^[0-9]+(-[0-9]+)*$", message = "전화번호 양식을 맞춰주세요.")
     String phone
 ) {
 
