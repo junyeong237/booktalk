3a090c48d6ff111133c6ad2b79b632a972e25537
kimgunwoo <guncow0603@gmail.com>
2024-02-01 11:11:36 +0900

    Refactor : 탈퇴한 회원일경우 프로필 조회 안되게 수정

--- src/main/java/com/example/booktalk/domain/front/FrontUserController.java
+++ src/main/java/com/example/booktalk/domain/front/FrontUserController.java
@@ -1,12 +1,16 @@
 package com.example.booktalk.domain.front;
 
+import com.example.booktalk.domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
+@RequiredArgsConstructor
 public class FrontUserController {
+    private final UserService userService;
 
     @GetMapping("/booktalk/users/profile")
     public String myProfilePage() {
@@ -15,6 +19,7 @@ public class FrontUserController {
 
     @GetMapping("/booktalk/users/{userId}/profile")
     public String otherProfilePage(@PathVariable Long userId, Model model) {
+        userService.UserWithdrawCheck(userId);
         model.addAttribute("userId", userId);
         return "otherProfile";
     }
